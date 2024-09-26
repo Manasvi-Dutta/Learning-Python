66b0084e6512c8213126e3d0cb5e1c702baa7ece
Manasvi-Dutta <manasvidutta13@gmail.com>
2024-09-26 14:12:13 +0530

    This was my first python project - Grocery store accounts

---
+++ Lecture-material2.py
@@ -0,0 +1,8 @@
+Penne =16.68    #16oz pack of 12
+ArraBiata =6.98  #24oz of pasta sauce
+bag = 16.78  #bag of 20 organic garlic cloves
+Italian = 15.26  #1.5oz bottle of italian seasoning
+Artisan = 3.00  #twin pack of baguettes
+bagOfMeatBalls = 4.39  #12oz bag of meatballs
+subtotal = round((Penne+ArraBiata+bag+Italian+bagOfMeatBalls), 2)  #subtotal is the sum of all prices
+print(subtotal)
